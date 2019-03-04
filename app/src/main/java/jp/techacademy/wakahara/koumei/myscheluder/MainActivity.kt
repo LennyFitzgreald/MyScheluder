89458dc2198595bda0df72fed91f945634fa068e
LennyFitzgerald <lennyfitzgerald0422@gmail.com>
2019-03-04 12:29:26 +0900

    Finished

--- app/src/main/java/jp/techacademy/wakahara/koumei/myscheluder/MainActivity.kt
+++ app/src/main/java/jp/techacademy/wakahara/koumei/myscheluder/MainActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import io.realm.Realm
+import io.realm.kotlin.where
+
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var realm: Realm     // あとからonCreate内で初期化するのでlateinit修飾子をつける
@@ -14,10 +18,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         realm = Realm.getDefaultInstance()   // Realmクラスのインスタンスを取得、Realmデータベースを使用する準備が完了
+        val schedules = realm.where<Schedule>().findAll()
+        listView.adapter = ScheduleAdapter(schedules)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            startActivity<ScheduleEditActivity>()
+        }
+
+        listView.setOnItemClickListener { parent, view, position, id ->
+            val schedule = parent.getItemAtPosition(position) as Schedule
+            startActivity<ScheduleEditActivity>(
+                "schedule_id" to schedule.id
+            )
         }
     }
 
